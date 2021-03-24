cdf7f7a26c99c8c13157afcf83598c3eff22d519
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-03-24 09:11:35 +0100

    upd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { Container } from "react-bootstrap";
 import {withRouter,Redirect} from "react-router-dom";
-import {Button} from "@material-ui/core"
-import axios from "axios"
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
