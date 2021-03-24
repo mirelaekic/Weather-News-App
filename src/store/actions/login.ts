72a3170bd0400f3ac66b4e281af08ed61f943ee4
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-03-24 08:36:58 +0100

    Update login.ts

--- src/store/actions/login.ts
+++ src/store/actions/login.ts
@@ -1,45 +1,45 @@
-import { ThunkAction } from "redux-thunk";
-import { RootState } from "../index";
-import axios from "axios";
+// import { ThunkAction } from "redux-thunk";
+// import { RootState } from "../index";
+// import axios from "axios";
 
-import {
-  LoginAction,
-  UserData,
-  LoginError,
-  LOGIN,
-  SET_LOADING,
-  SET_ERROR,
-} from "../types";
+// import {
+//   LoginAction,
+//   UserData,
+//   LoginError,
+//   LOGIN,
+//   SET_LOADING,
+//   SET_ERROR,
+// } from "../types";
 
-export const login = (
-  email: string,
-  password: string
-): ThunkAction<void, RootState, null, LoginAction> => {
-  return() => {
-    axios
-      .post("http://localhost:3012/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          email,
-          password,
-        },
-        withCredentials: true, // use cookies
-      })
-      localStorage.setItem("user",email)
-  };
-};
-export const setLoading = (): LoginAction => {
-  return {
-    type: SET_LOADING,
-  };
-};
+// export const login = (
+//   email: string,
+//   password: string
+// ): ThunkAction<void, RootState, null, LoginAction> => {
+//   return() => {
+//     axios
+//       .post("http://localhost:3012/users/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         data: {
+//           email,
+//           password,
+//         },
+//         withCredentials: true, // use cookies
+//       })
+//       localStorage.setItem("user",email)
+//   };
+// };
+// export const setLoading = (): LoginAction => {
+//   return {
+//     type: SET_LOADING,
+//   };
+// };
 
-export const setError = (): LoginAction => {
-  return {
-    type: SET_ERROR,
-    payload: "",
-  };
-};
+// export const setError = (): LoginAction => {
+//   return {
+//     type: SET_ERROR,
+//     payload: "",
+//   };
+// };
