877663f330f853a427bea3ccac24c8a62ec3c686
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-03-24 08:27:59 +0100

    reverted

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,17 +6,13 @@ import weatherReducer from './reducers/weatherReducer';
 import alertReducer from './reducers/alertReducer';
 import imageReducer from './reducers/imageReducer';
 import sevenDayReducer from './reducers/sevenDayReducer';
-import newsReducer from './reducers/newsReducer';
-import loginReducer from './reducers/loginReducer';
-import registerReducer from './reducers/registerReducer'
+import newsReducer from './reducers/newsReducer'
 const rootReducer = combineReducers({
   weather: weatherReducer,
   alert: alertReducer,
   image:imageReducer,
   sevenDayWeather:sevenDayReducer,
   news:newsReducer,
-  login:loginReducer,
-  register:registerReducer
 });
 
 const store = createStore(
